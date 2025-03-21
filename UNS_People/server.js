8a5b015976f8d95681d41e8d1b39a1de037ae5f6
Hanzo7771 <hanzlaasif8@gmail.com>
2025-03-22 00:08:54 +0500

    Add files via upload

--- UNS_People/server.js
+++ UNS_People/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 // Serve static files from the "public" folder
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Handle socket connections
 io.on("connection", (socket) => {
